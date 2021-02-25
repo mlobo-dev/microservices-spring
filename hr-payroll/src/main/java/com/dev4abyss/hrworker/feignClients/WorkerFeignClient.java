ade59128dafb8fdbf82b0cd5c69dccf9c6d513d4
mlobo.dev <mlobo.dev@gmail.com>
2021-02-25 20:25:16 -0300

    add load balance with ribbon

--- hr-payroll/src/main/java/com/dev4abyss/hrworker/feignClients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/dev4abyss/hrworker/feignClients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "hr-worker", url = "${api.worker.url}", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
 
--- hr-worker/src/main/java/com/dev4abyss/hrworker/resources/WokerResource.java
+++ hr-worker/src/main/java/com/dev4abyss/hrworker/resources/WokerResource.java
@@ -7,6 +7,8 @@ import com.dev4abyss.hrworker.services.WorkerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @Api(value = "Workers", tags = "Workers")
 @RequestMapping("/workers")
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class WokerResource {
 
     private final WorkerService service;
     private final WorkerMapper mapper;
+    private final Environment env;
 
 
     @ApiOperation("List all workers")
@@ -34,6 +38,7 @@ public class WokerResource {
     @ApiOperation("List worker by id")
     @GetMapping("/{id}")
     public ResponseEntity<WorkerDTO> findById(@PathVariable Long id) {
+        log.info("PORT" + env.getProperty("local.server.port"));
         return ResponseEntity.ok(mapper.toDto(service.findById(id)));
     }
 }
