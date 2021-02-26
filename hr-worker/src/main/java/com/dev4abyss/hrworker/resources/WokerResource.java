cbd0a0e516a85ba12f7aef85b8c9491410e72227
mlobo.dev <mlobo.dev@gmail.com>
2021-02-26 19:16:13 -0300

    Testing config server

--- hr-worker/src/main/java/com/dev4abyss/hrworker/resources/WokerResource.java
+++ hr-worker/src/main/java/com/dev4abyss/hrworker/resources/WokerResource.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,10 +30,21 @@ public class WokerResource {
     private final Environment env;
 
 
+    @Value("${test.config}")
+    private String config;
+
+
     @ApiOperation("List all workers")
     @GetMapping
     public ResponseEntity<List<WorkerDTO>> listAll() {
         return ResponseEntity.ok(mapper.toDto(service.listAll()));
+    }
+
+    @ApiOperation("List all workers")
+    @GetMapping("/configs")
+    public ResponseEntity<String> configs() {
+        log.info(config);
+        return ResponseEntity.ok(config);
     }
 
     @ApiOperation("List worker by id")
