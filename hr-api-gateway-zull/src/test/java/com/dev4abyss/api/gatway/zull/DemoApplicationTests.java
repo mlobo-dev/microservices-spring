3f4ed669afde75b6c531c483fcdcfadbbc51fc2c
mlobo.dev <mlobo.dev@gmail.com>
2021-02-26 06:28:38 -0300

    api-gateway-zull created

---
+++ hr-api-gateway-zull/src/test/java/com/dev4abyss/api/gatway/zull/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4abyss.api.gatway.zull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
