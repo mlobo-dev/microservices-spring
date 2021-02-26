7f4b02b89dc798033ad3c273be8963bbc1a7c206
mlobo.dev <mlobo.dev@gmail.com>
2021-02-26 18:59:31 -0300

    configuring hr-config-server

---
+++ hr-config-server/src/main/java/com/dev4abyss/configserver/HrConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.dev4abyss.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication.class, args);
+    }
+
+}
