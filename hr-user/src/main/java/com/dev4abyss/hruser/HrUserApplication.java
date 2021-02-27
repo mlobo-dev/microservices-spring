47e2017715aa464b829a1dd49debc42059a7a793
mlobo.dev <mlobo.dev@gmail.com>
2021-02-27 19:01:03 -0300

    database seeding

--- hr-user/src/main/java/com/dev4abyss/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/dev4abyss/hruser/HrUserApplication.java
@@ -1,15 +1,25 @@
 package com.dev4abyss.hruser;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+	private final BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("Bcrypt = "+passwordEncoder.encode("123456"));
+	}
 }
