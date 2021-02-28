6773beab93c5c82fb5aecd281303cb32cec788d9
mlobo.dev <mlobo.dev@gmail.com>
2021-02-28 07:43:12 -0300

    Implementing oauth2

--- hr-oauth/src/main/java/com/dev4abyss/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/dev4abyss/hroauth/entities/User.java
@@ -4,14 +4,19 @@ package com.dev4abyss.hroauth.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
+public class User implements UserDetails {
 
 
     private Long id;
@@ -19,4 +24,36 @@ public class User {
     private String email;
     private String password;
     private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- hr-oauth/src/main/java/com/dev4abyss/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/dev4abyss/hroauth/services/UserService.java
@@ -4,15 +4,17 @@ import com.dev4abyss.hroauth.entities.User;
 import com.dev4abyss.hroauth.exception.ObjectNotFoundException;
 import com.dev4abyss.hroauth.exception.OperationException;
 import com.dev4abyss.hroauth.feigns.UserFeignClient;
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserFeignClient userFeignClient;
 
@@ -29,5 +31,18 @@ public class UserService {
         } catch (Exception e) {
             throw new OperationException("failed to recover user", e);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userFeignClient.findById(username).getBody();
+        if (user == null) {
+            log.error("email not found: " + username);
+            throw new UsernameNotFoundException("User not found by email: " + username);
+        }
+        log.info("Email found: " + username);
+        return user;
+
     }
 }
