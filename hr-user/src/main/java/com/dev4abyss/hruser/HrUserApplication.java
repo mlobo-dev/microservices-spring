6c0981ed2ff8a9238040eabb099f6d8a3853772e
mlobo.dev <mlobo.dev@gmail.com>
2021-03-02 06:09:16 -0300

    Configuring hr-user to read config server properties

--- hr-user/src/main/java/com/dev4abyss/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/dev4abyss/hruser/HrUserApplication.java
@@ -5,13 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@RequiredArgsConstructor
+
 @EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication implements CommandLineRunner {
 
-//	SINGLETON
-//	private final BCryptPasswordEncoder passwordEncoder;
+
 
     public static void main(String[] args) {
         SpringApplication.run(HrUserApplication.class, args);
@@ -19,6 +18,5 @@ public class HrUserApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//		System.out.println("Bcrypt = "+passwordEncoder.encode("123456"));
     }
 }
